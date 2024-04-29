4b129b91b55dc2f7ac39aa4c392492038da0e505
Faris Fadhail <farisfadhail@gmail.com>
2024-04-29 23:52:13 +0700

    [fix] update route api

--- routes/api.go
+++ routes/api.go
@@ -21,23 +21,23 @@ func RouteInit(app *fiber.App) {
 	user := api.Group("/user", middleware.Authenticated, middleware.IsAdmin)
 	user.Get("/", handler.UserHandlerGetAll).Name("user.index")
 	user.Get("/:userId", handler.UserHandlerShowById).Name("user.show")
-	user.Put("/:userId/update", handler.UserHandlerUpdate).Name("user.update")
-	user.Delete("/:userId", handler.UserHandlerDelete).Name("user.destroy")
-	api.Put("/user/:userId/update-email", handler.UserHandlerUpdateEmail, middleware.Authenticated).Name("user.update-email")
+	user.Put("/update?user=:userId", handler.UserHandlerUpdate).Name("user.update")
+	user.Delete("/delete?user=:userId", handler.UserHandlerDelete).Name("user.destroy")
+	user.Put("/update?user=:userId&type=email", handler.UserHandlerUpdateEmail, middleware.Authenticated).Name("user.update-email")
 
 	// Question Routes
 	question := api.Group("/question", middleware.Authenticated)
 	question.Get("/", handler.QuestionHandlerGetAll).Name("question.index")
 	question.Post("/create", handler.QuestionHandlerCreate).Name("question.store")
 	question.Get("/:questionId", handler.QuestionHandlerShowById).Name("question.show")
-	question.Put("/:questionId/update", handler.QuestionHandlerUpdate).Name("question.update")
-	question.Delete("/:questionId", handler.QuestionHandlerDelete).Name("question.destroy")
+	question.Put("/update?question=:questionId", handler.QuestionHandlerUpdate).Name("question.update")
+	question.Delete("/delete?question=:questionId", handler.QuestionHandlerDelete).Name("question.destroy")
 
 	// Answer Routes
 	answer := api.Group("/answer", middleware.Authenticated)
 	answer.Get("/", handler.AnswerHandlerGetAll).Name("answer.index")
 	answer.Post("/create", handler.AnswerHandlerCreate).Name("answer.store")
 	answer.Get("/:answerId", handler.QuestionHandlerShowById).Name("answer.show")
-	answer.Put("/:answerId/update", handler.AnswerHandlerUpdate).Name("answer.update")
-	answer.Delete(":answerId", handler.AnswerHandlerDelete).Name("answer.destroy")
+	answer.Put("/update?answer=:answerId", handler.AnswerHandlerUpdate).Name("answer.update")
+	answer.Delete("/delete?answer=:answerId", handler.AnswerHandlerDelete).Name("answer.destroy")
 }
